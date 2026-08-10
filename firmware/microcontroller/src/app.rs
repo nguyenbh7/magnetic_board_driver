2ef1761e8b94ea54f46cbf1d210b0f3b3467020f
Brandon Nguyen <nguyenbh7@gmail.com>
2026-08-09 19:31:41 -0500

    Make MLX sensitivity read endpoint asynchronous

--- firmware/microcontroller/src/app.rs
+++ firmware/microcontroller/src/app.rs
@@ -39,7 +39,7 @@ use postcard_rpc::{
     },
 };
 use embassy_sync::mutex::Mutex;
-use crate::N; 
+use crate::N;
 use {defmt_rtt as _, panic_probe as _};
 
 type SensorGroupDefault = Mutex<CriticalSectionRawMutex, SensorGroup<
@@ -81,40 +81,18 @@ pub type AppTx = EioWireTx<CriticalSectionRawMutex, Tx>;
 
 /// AppRx is the type of our receiver, which is how we receive information from the client
 pub type AppRx = WireRxImpl<Rx>;
-/// AppServer is the type of the postcard-rpc server we are using
+/// AppServer is the type of our postcard-rpc server
 pub type AppServer = Server<AppTx, AppRx, WireRxBuf, MyApp>;
 
 pub static STORAGE: Storage = Storage::new();
 define_dispatch! {
-    // You can set the name of your app to any valid Rust type name. We use
-    // "MyApp" here. You'll use this in `main` to create an instance of the
-    // app.
     app: MyApp;
-    // This chooses how we spawn functions. Here, we use the implementation
-    // from the `embassy_usb_v0_4` implementation
     spawn_fn: spawn_fn;
-    // This is our TX impl, which we aliased above
     tx_impl: AppTx;
-    // This is our spawn impl, which also comes from `embassy_usb_v0_4`.
     spawn_impl: WireSpawnImpl;
-    // This is the context type we defined above
     context: Context;
 
-    // Endpoints are how we handle request/response pairs from the client.
-    //
-    // The "EndpointTy" are the names of the endpoints we defined in our ICD
-    // crate. The "kind" is the kind of handler, which can be "blocking",
-    // "async", or "spawn". Blocking endpoints will be called directly.
-    // Async endpoints will also be called directly, but will be await-ed on,
-    // allowing you to call async functions. Spawn endpoints will spawn an
-    // embassy task, which allows for handling messages that may take some
-    // amount of time to complete.
-    //
-    // The "handler"s are the names of the functions (or tasks) that will be
-    // called when messages from this endpoint are received.
     endpoints: {
-        // This list comes from our ICD crate. All of the endpoint handlers we
-        // define below MUST be contained in this list.
         list: ENDPOINT_LIST;
 
         | EndpointTy                | kind      | handler                       |
@@ -123,28 +101,19 @@ define_dispatch! {
         | SingleFieldValue          | async     | single_request_handler        |
         | StartFieldStream          | spawn     | stream_field                  |
         | StopFieldStream           | blocking  | stop_stream                   |
-        | GetMlxSensitivity         | blocking  | get_mlx_sensitivity_handler   |
-        | SetMlxSensitivity         | async  | set_mlx_sensitivity_handler   |
-        | GetBoardPresence        | async     | get_board_presence_handler |
+        | GetMlxSensitivity         | async     | get_mlx_sensitivity_handler   |
+        | SetMlxSensitivity         | async     | set_mlx_sensitivity_handler   |
+        | GetBoardPresence          | async     | get_board_presence_handler    |
     };
 
-    // Topics IN are messages we receive from the client, but that we do not reply
-    // directly to. These have the same "kinds" and "handlers" as endpoints, however
-    // these handlers never return a value
     topics_in: {
-        // This list comes from our ICD crate. All of the topic handlers we
-        // define below MUST be contained in this list.
         list: TOPICS_IN_LIST;
 
         | TopicTy                   | kind      | handler                       |
         | ----------                | ----      | -------                       |
     };
 
-    // Topics OUT are the messages we send to the client whenever we'd like. Since
-    // these are outgoing, we do not need to define handlers for them.
     topics_out: {
-        // This list comes from our ICD crate.
         list: TOPICS_OUT_LIST;
-        
     };
 }
